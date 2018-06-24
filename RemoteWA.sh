81f83eb1a131148cb2079f28ceddb443b2995759
root <root@localhost.localdomain>
2018-06-24 03:09:58 +0100

    Comienzo del programa

---
+++ RemoteWA.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+logoSpawn()
+{
+	echo -e "${yellowColour}╭━━━╮╱╱╱╱╱╱╱╱╭╮╱╱╱╭╮╭╮╭┳━━━╮" && sleep 0.03
+	echo -e "┃╭━╮┃╱╱╱╱╱╱╱╭╯╰╮╱╱┃┃┃┃┃┃╭━╮┃" && sleep 0.03
+	echo -e "┃╰━╯┣━━┳╮╭┳━┻╮╭╋━━┫┃┃┃┃┃┃╱┃┃ (${endColour}${grayColour}Hecho por ${greenColour}Marcelo Vázquez$endColour$grayColour aka${endColour}${redColour} s4vitar${endColour}$yellowColour)" && sleep 0.03
+	echo -e "┃╭╮╭┫┃━┫╰╯┃╭╮┃┃┃┃━┫╰╯╰╯┃╰━╯┃" && sleep 0.03
+	echo -e "┃┃┃╰┫┃━┫┃┃┃╰╯┃╰┫┃━╋╮╭╮╭┫╭━╮┃" && sleep 0.03
+	echo -e "╰╯╰━┻━━┻┻┻┻━━┻━┻━━╯╰╯╰╯╰╯╱╰╯${endColour}" & sleep 0.4
+}
+
+if [ "$(id -u)" -eq "0" ]; then
+	tput civis && clear
+	logoSpawn
+else
+	echo -e "${redColour}Es necesario correr el programa como root${endColour}"
+fi
